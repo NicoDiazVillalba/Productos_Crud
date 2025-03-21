5389a5f51809f397ebca96c0e128e9a8256b11bf
NicoDiazVillalba <90287672+NicoDiazVillalba@users.noreply.github.com>
2025-03-20 20:16:42 -0500

    Se agrega confirmación en el Script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const deleteLinks = document.querySelectorAll(".delete");
+
+    deleteLinks.forEach(link => {
+        link.addEventListener("click", function (event) {
+            if (!confirm("¿Estás seguro de que deseas eliminar este producto?")) {
+                event.preventDefault();
+            }
+        });
+    });
+});
