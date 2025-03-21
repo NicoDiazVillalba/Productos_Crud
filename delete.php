cc65f09bb670a3d63eb09bc2dcd60d6343fde2c6
NicoDiazVillalba <90287672+NicoDiazVillalba@users.noreply.github.com>
2025-03-20 19:22:00 -0500

    Se agregan las funcionalidades del CRUD

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+include 'conection.php';
+
+$id = $_GET['id'];
+$sql = "DELETE FROM productos WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: index.php");
+    exit();
+}
+?>
